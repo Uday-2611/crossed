757c8b7387f092a1ebb3eb570c4167cf6cdebca7
Uday <udayagarwal234@gmail.com>
2025-12-18 00:47:54 +0530

    Implement Navigation Bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,16 @@
+import { CustomTabBar } from '@/components/CustomTabBar';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="matches"
-        options={{
-          tabBarAccessibilityLabel: 'Matches tab',
-        }}
-      />
-      <Tabs.Screen
-        name="chats"
-        options={{
-          tabBarAccessibilityLabel: 'Chats tab',
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarAccessibilityLabel: 'Profile tab',
-        }}
-      />    </Tabs>
+     <Tabs
+      screenOptions={{ headerShown: false }}
+      tabBar={(props) => <CustomTabBar {...props} />}
+    >
+      <Tabs.Screen name="matches" />
+      <Tabs.Screen name="chats" />
+      <Tabs.Screen name="profile" />
+    </Tabs>
   );
 }
